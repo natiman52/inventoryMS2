66305baeb4a0a0c248bd0d7ba972c257fee76211
MyICTProject <natnaelyazchew135210@gmail.com>
2025-08-17 23:46:08 +0300

    fixing

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -142,7 +142,7 @@ LOGOUT_URL = 'logout'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = "home/nahom/InventoryMS/staticroot"
+STATIC_ROOT = "/home/nahom/inventoryMS2/staticroot"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
